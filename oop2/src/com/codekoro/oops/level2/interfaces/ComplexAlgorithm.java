7d8bfc31cec37474a562a22b4f0bdd316774f839
King-James-II <105425391+King-James-II@users.noreply.github.com>
2025-03-30 21:42:06 -0400

    Delete oop2 directory

--- oop2/src/com/codekoro/oops/level2/interfaces/ComplexAlgorithm.java
+++
@@ -1,7 +0,0 @@
-package com.codekoro.oops.level2.interfaces;
-
-public interface ComplexAlgorithm {
-
-	int complexAlgorithm(int number1, int number2);
-
-}
--- oop2/src/com/codekoro/oops/level2/interfaces/GamingConsole.java
+++
@@ -1,9 +0,0 @@
-package com.codekoro.oops.level2.interfaces;
-
-public interface GamingConsole {
-	public void up();
-	public void down();
-	public void left();
-	public void right();
-	
-}
